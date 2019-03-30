71743804a40515ee43968735a6e06563999397ed
Matt Gill <mtgill90@outlook.com>
2019-03-30 11:46:31 -0500

    age verification page functional, just needs to be restyled

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log(`setup is working`);
+
+
+// function for age verification buttons
+const ageButtonActions = (e) => {
+    targetId = e.target.id;
+    if(targetId === 'noButton'){
+        document.location.href = 'https://www.sesamestreet.org/';
+    }
+    else if(targetId === 'yesButton'){
+        document.location.href = './html/home.html';
+    }
+}
+
+//add event listeners to age verification buttons
+const ageButtonEvents = () => {
+    ageButtons = document.getElementsByClassName('ageButton');
+    for(let i = 0; i < ageButtons.length; i++){
+        ageButtons[i].addEventListener('click', ageButtonActions);
+    }
+}
+
+const init = () => {
+    ageButtonEvents();
+}
+
+init();
