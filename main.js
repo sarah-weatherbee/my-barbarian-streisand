f138ec91cd828b70cea5457a7e02cf9be46ea0b6
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-30 11:26:11 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log(`setup is working`);
+
+
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+tourForm = () => {
+    let domString = `<p>Form</p>`;
+
+    printToDom("tour-form", domString);
+};
+
+const buttonEvents = () => {
+    document.getElementById("book-tour-button").addEventListener("click", tourForm);
+
+const init = () => {
+    buttonEvents();
+  
+};
+
+ init();
